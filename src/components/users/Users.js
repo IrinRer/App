9d7be97d9e0554fa8e35afb5e2cedc6d3d41623e
Irin <iraantonova1998@list.ru>
2022-01-28 21:18:03 +0300

    les 50 axious

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,24 +1,47 @@
 import classes from "./Users.module.scss";
+import img from "./img/search.svg";
+import avatar from "./img/atomic.png";
+import * as axios from "axios";
 
-const Users = ({ users, followed, setUser}) => {
+const Users = ({ users, followed, setUser }) => {
+  if (users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+       setUser(response.data.items);
+      });
+  }
   const element = users.map((item) => {
+      debugger;
     return (
-      <div key={item.id}>
+      <div key={item.id} className={classes.wrapper}>
+        <div className={classes.wrapper__img}>
+          <img src={item.photos.small != null ? item.photos.small : avatar}/>
+          <button onClick={() => followed(item.id)}>
+            {item.followed ? "Follow" : "Unfollow"} 
+          </button>
+        </div>
         <div className={classes.wrapper__inf}>
           <h3>{item.name}</h3>
           <h3>{item.country}</h3>
           <h3>{item.city}</h3>
-          <p>{item.status}</p>
-        </div>
-        <div className={classes.wrapper__img}>
-          <img src={item.thumbnail}></img>
-          <button onClick={() => followed(item.id)}>{ item.followed ? 'Follow' : 'Unfollow'}</button>
+          <p>{item.description}</p>
         </div>
       </div>
     );
   });
-  debugger;
-  return <div className={classes.wrapper}>{element}</div>;
+
+  return (
+    <>
+      <form className={classes.form}>
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <img src={img} alt="icon search" />
+        </button>
+      </form>
+      {element}
+    </>
+  );
 };
 
 export default Users;
