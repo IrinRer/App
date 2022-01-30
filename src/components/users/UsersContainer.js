ca5243fdc1255f98d9ef68dc0587ed25328d2522
Irin <iraantonova1998@list.ru>
2022-01-30 23:17:42 +0300

    less.58 load

--- src/components/users/UsersContainer.js
+++ src/components/users/UsersContainer.js
@@ -1,26 +1,84 @@
+import { Component } from "react";
 import { connect } from "react-redux";
-import {followActionCreater, setUsersActionCreater, 
-setCurrentActionCreater, setUserCountActionCreater} from '../../redux/UsersReducer';
-import UsersC from "./UsersC";
+import * as axios from "axios";
+import {
+  followActionCreater,
+  setUsersActionCreater,
+  setCurrentActionCreater,
+  setUserCountActionCreater,
+  loadToggleActionCreater
+} from "../../redux/UsersReducer";
+import Users from "./Users";
+import classes from "./Users.module.scss";
+import Spinner from "../spinner/Spinner";
 
+class UsersAPI extends Component {
+  componentDidMount() {
+    this.props.loadToggle(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currenPage}&
+            count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.loadToggle(false)
+        this.props.setUser(response.data.items);
+        this.props.setUserCount(response.data.totalCount);
+      });
+  }
+
+  onPageChange = (p) => {
+    this.props.setPage(p);
+    this.props.loadToggle(true);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${p}&
+          count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.loadToggle(false);
+        this.props.setUserCount(response.data.totalCount);
+        this.props.setUser(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <>
+        {this.props.isLoad ? <Spinner className={classes.load}/>:  <Users
+          users={this.props.users}
+          followed={this.props.followed}
+          pageSize={this.props.pageSize}
+          totalUsersCount={this.props.totalUsersCount}
+          currenPage={this.props.currenPage}
+          onPageChange={this.onPageChange}
+        />}
+       
+      </>
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
-    return {
-      users: state.users.users,
-      pageSize: state.users.pageSize,
-      totalUsersCount: state.users.totalUsersCount,
-      currenPage: state.users.currenPage
-    }
+  return {
+    users: state.users.users,
+    pageSize: state.users.pageSize,
+    totalUsersCount: state.users.totalUsersCount,
+    currenPage: state.users.currenPage,
+    isLoad: state.users.isLoad,
   };
-  
-  let mapDispathToProps = (dispatch) => {
-    return {
-      followed: id => dispatch(followActionCreater(id)),
-      setUser: users => dispatch(setUsersActionCreater(users)),
-      setPage: page => dispatch(setCurrentActionCreater(page)),
-      setUserCount: count => dispatch(setUserCountActionCreater(count))
-    }
+};
+
+let mapDispathToProps = (dispatch) => {
+  return {
+    followed: (id) => dispatch(followActionCreater(id)),
+    setUser: (users) => dispatch(setUsersActionCreater(users)),
+    setPage: (page) => dispatch(setCurrentActionCreater(page)),
+    setUserCount: (count) => dispatch(setUserCountActionCreater(count)),
+    loadToggle: (load) => dispatch(loadToggleActionCreater(load))
   };
-  
-  const UsersContainer = connect(mapStateToProps, mapDispathToProps)(UsersC)
-  export default UsersContainer;
+};
+
+const UsersContainer = connect(mapStateToProps, mapDispathToProps)(UsersAPI);
+
+export default UsersContainer;
--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -2,12 +2,14 @@ const FOLLOW_TOGGLE = 'FOLLOW_TOGGLE';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_COUNT = 'SET_COUNT';
+const LOAD_TOGGLE = 'LOAD_TOGGLE';
 
 let initState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
-    currenPage: 1
+    currenPage: 1,
+    isLoad: false
 }
 
 export const UsersReducer = (state = initState, action) => {
@@ -43,6 +45,12 @@ export const UsersReducer = (state = initState, action) => {
             totalUsersCount: action.totalUsersCount
         }
 
+        case LOAD_TOGGLE: 
+        return {
+            ...state,
+            isLoad: action.isLoad
+        }
+
        default: 
        return state;
    }
@@ -73,5 +81,12 @@ export const setUserCountActionCreater = totalUsersCount => {
     return {
         type: SET_COUNT,
         totalUsersCount
+    }
+};
+
+export const loadToggleActionCreater = (load) => {
+    return {
+        type: LOAD_TOGGLE,
+        isLoad: load
     }
 };
