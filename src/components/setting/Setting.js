d32fd1c724e78ec7d7a13d0d761c216e1fead855
Irin <iraantonova1998@list.ru>
2022-02-08 22:15:36 +0300

    change style setting

--- src/components/setting/Setting.js
+++ src/components/setting/Setting.js
@@ -3,7 +3,11 @@ import classes from './Setting.module.scss';
 const Setting = () => {
     return (
         <div>
-            <h3>Setting</h3>
+            <h2>Choose your theme</h2>
+            <div className={classes.changeTheme}>
+                <button className={classes.buttonDark}>Go to the dark side</button>
+                <button className={classes.buttonLight}>Go to the light side</button>
+            </div>
         </div>
     );
 };
